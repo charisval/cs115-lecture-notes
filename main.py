be67c3462d62a612829373682747dde83a19fbb0
charisval <charischerryberry@gmail.com>
2025-02-19 19:15:12 +0000

    we've added to main.py a bunch of examples on how to code in Python

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Hello world")
+
+my_variable = 6
+num_cats = 8
+print("num cats: ", num_cats)
+print(f"number of cats: {num_cats}")
+print(f"my age {my_variable} and my cats {num_cats}")
+
+x = 6
+y = 20
+c = x + y 
+print(c)
+print(f"c = {x+y}")
+print(f"x*y = {x**y}")
+
+# single line comment
+"""
+Multi line comment
+"""
+
+num1 = input("First number: ")
+num2 = input("Second number: ")
+sum = int(num1) + int(num2)
+# int function changes the strings into integers
+print(f"{num1} + {num2} = {sum}")
